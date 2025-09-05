8b51c40681896d96ffb0c9bd7d995aef446e4bbb
rick chen <admin@rickruguichen.altostrat.com>
2025-09-05 18:10:09 +0000

    update tp-comm-overlap for testing

--- nemo_llama2_70b_lora/config_XE9680lx8H200-SXM-141GB_1x8x2xtp1pp1cp2.sh
+++ nemo_llama2_70b_lora/config_XE9680lx8H200-SXM-141GB_1x8x2xtp1pp1cp2.sh
@@ -1,6 +1,7 @@
 #source $(dirname ${BASH_SOURCE[0]})/config_common.sh
 
 # hyperparameters
+# hyperparameters
 export MAX_STEPS=896
 export LR=0.0005
 export MINIBS=2
@@ -23,22 +24,6 @@ export UCX_TLS=self,tcp
 export DGXNNODES=1
 export WALLTIME_RUNANDTIME=UNLIMITED
 export WALLTIME=UNLIMITED
-
-
-export FP8=True
-export FP8_AMAX_ALGO=max
-export FP8_REDUCE_AMAX=True
-export FP8_AMAX_HISTORY=4
-export FP8_DPA=1
-
-export PP=2 #1
-export CG_WEIGHT_CACHING=0
-
-export MBS=1
-export VAL_MBS=1
-export VAL_CHECK_INTERVAL=384
-export TORCH_CPP_LOG_LEVEL=ERROR
-export LOAD_CKPT=True
 
 #export WALLTIME_RUNANDTIME=50
 #export WALLTIME=$((5 + ${NEXP:-1} * ($WALLTIME_RUNANDTIME + 5)))
